fcb238f70c0a14c6f6e7efe0a92e675eac4d33b9
pearce <pearce712134@gmail.com>
2022-06-15 22:41:06 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-gcache
+
+go 1.17
+
+require github.com/bluele/gcache v0.0.2 // indirect
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bluele/gcache"
+)
+
+func main() {
+
+	gc := gcache.New(20).LRU().Build()
+
+	gc.Set("key", "ok")
+
+	value, err := gc.Get("key")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println("Get: ", value)
+}
